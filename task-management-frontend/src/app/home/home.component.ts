1825c4a248413f9ce15667ed471f99b365523cc4
somar sabe <sabesofteng@gmail.com>
2023-12-22 10:42:45 +0400

    somar

--- task-management-frontend/src/app/home/home.component.ts
+++ task-management-frontend/src/app/home/home.component.ts
@@ -6,15 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  userName: string = 'Guest'; // Initialize userName with default value 'Guest'
+  userName: string = 'Guest'; 
 
   constructor() {
-    // Logic to check if the user is authenticated (for demonstration purposes)
-    const isAuthenticated = /* Perform authentication check */ true;
+    
+    const isAuthenticated =  true;
 
     if (isAuthenticated) {
-      // If authenticated, set userName to the user's name (assuming it's fetched from authentication)
-      this.userName = 'John Doe'; // Replace 'John Doe' with the actual user's name
+      
+      this.userName = 'somar sabe'; 
     }
   }
 }
